01bdad2556a1aa52e9e47c8499ead79f00361a30
Nasywa <itsmenasywa@gmail.com>
2021-08-24 23:10:49 +0700

    [feature] add blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,21 +9,28 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "images",
+    //     path: "./src/images/",
+    //   },
+    //   __key: "images",
+    // },
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "pages",
+    //     path: "./src/pages/",
+    //   },
+    //   __key: "pages",
+    // },
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
     },
   ],
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>Aku nasywa nur f ini random image yg aku screenshost</p>
+      <StaticImage
+        alt="coba upload"
+        src="../images/logo-sevvy.png"
+      />
     </Layout>
   )
 }
